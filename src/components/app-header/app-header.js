272c6eeb8feaabacb7908e2ee420de7b0ae36c1a
Yura O <yurkasoft@gmail.com>
2021-04-21 17:14:04 +0300

    go

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app-header.css'
+
+function AppHeader() {
+  return (
+    <div className="girl-and-pet">
+
+    </div>
+  );
+}
+
+export default AppHeader;
+
---
+++ src/components/app/app.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import AppHeader from '../app-header';
+
+
+import './app.css';
+import './index.css';
+
+export default class App extends Component {
+
+    render() {
+
+        return (
+            
+        <div className="app"> 
+        
+        <div className="group25">   </div>
+             
+        <div className="MaskGroup0"> </div>
+        <div className="Rectangle13" ></div>
+        <div className="Rectangle22" ></div>
+        <div className="vote-table" ></div>
+        
+        
+        <div className="MaskGroup1"></div>
+        <div className="Rectangle13a" ></div>
+        <div className="Rectangle22a" ></div>
+        <div className="pet-breeds" ></div>
+        
+
+        <div className="MaskGroup2"></div> 
+        <div className="Rectangle13b" ></div>
+        <div className="Rectangle22b" ></div>
+        <div className="images-search" ></div>
+        
+        
+        <div className="HiIntern">Hi intern!</div>
+
+        <div className="FrontTest">Welcome to MSI 2021 Front-end test</div>
+
+        <div className="DogsAPI">Lets start using The Dogs API</div>
+        
+        
+        <div className="Logo">
+        <div className="PetsPaw"><div>PetsPaw</div></div>
+        
+        
+
+        <div className="Group20"></div>
+        <div className="Ellipse4"></div>
+        <div className="Ellipse5"></div>
+        <div className="Ellipse6"></div>
+        <div className="Ellipse3"></div>
+        
+        <div className="Rectangle11"></div>
+        <div className="Rectangle12"></div>
+        
+        </div>
+        
+        
+        
+        
+       
+       
+        
+        
+
+        
+        
+        
+       
+        
+
+        <div className="rectangle"></div>
+        <div className="girl-and-pet"></div>    
+        
+           
+        </div>
+        
+        )
+    } 
+       
+        
+            
+               
+}
